493321f51f47aef89ac760cada5d18a16454d6cb
light885 <lenya074@gmail.com>
2021-10-12 02:30:48 +0200

    added slider section and fixed header

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,15 @@
 $(function () {
-  
+  $('.promotion__inner').slick({
+    dots: false,
+
+    prevArrow: '<button type="button" class="slick-prev"><img src="images/icon/arrow_left.svg" alt="стрелка влево"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="images/icon/arrow_right.svg" alt="стрелка вправо"></button>',
+
+    arrows: true,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 2000   
+
+
+  })
 });
