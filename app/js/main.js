cc49bd1ec8a26c1ef0923b1348ba9ac059d0125d
light885 <lenya074@gmail.com>
2021-10-16 22:16:15 +0200

    added banners

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,7 @@
 $(function () {
   $('.promotion__inner').slick({
     dots: false,
-    
+
     nextArrow: '<button type="button" class="slick-next"><img src="images/icon/arrow_right.svg" alt="стрелка вправо"></button >',
     prevArrow: '<button type="button" class="slick-prev"><img src="images/icon/arrow_left.svg" alt="стрелка влево"></button>',
 
@@ -11,6 +11,17 @@ $(function () {
     autoplaySpeed: 2000
 
   });
+
+  $('.brands__list').slick({
+    infinite: true,
+    slidesToShow: 6,
+    slidesToScroll: 3,
+    arrows: false,
+    Speed: 2000  
+
+  });
+
+
 
   var mixer = mixitup('.product__content');
 
